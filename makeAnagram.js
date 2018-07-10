0d2e46df304dab900badceb7ad349c6eb7191a21
lee-philip-n <lee.philip.n@gmail.com>
2018-07-10 13:35:58 -0700

    added comments

--- makeAnagram.js
+++ makeAnagram.js
@@ -3,10 +3,12 @@ function makeAnagram(a, b) {
   const bObj = {};
   const storage = {};
 
+  //created character count for a string
   for (let i = 0; i < a.length; i++) {
     aObj[a[i]] ? aObj[a[i]]++ : aObj[a[i]] = 1;
   }
 
+  //create character count for b string
   for (let i = 0; i < b.length; i++) {
     bObj[b[i]] ? bObj[b[i]]++ : bObj[b[i]] = 1;
   }  
@@ -14,13 +16,19 @@ function makeAnagram(a, b) {
   const aKeys = Object.keys(aObj)
   const bKeys = Object.keys(bObj)
 
+  //remove the characters that can from an anagram by...
+  //checking which has more unique characters
   if (bKeys.length > aKeys.length || bKeys.length === aKeys.length) {
     for (let i = 0; i < bKeys.length; i++) {
+      //if letter is in both a and b objects
       if (aKeys.includes(bKeys[i])) {
+        //compare the number of times the letter appear in the string
         if (bObj[bKeys[i]] === aObj[bKeys[i]]) {
+          //if same, remove the count from both
           bObj[bKeys[i]] = 0;
           aObj[bKeys[i]] = 0;
         } else {
+          //if diff, find the abs value of the difference
           bObj[bKeys[i]] = Math.abs(bObj[bKeys[i]] - aObj[bKeys[i]]);
           aObj[bKeys[i]] = 0;          
         }
@@ -28,6 +36,7 @@ function makeAnagram(a, b) {
     }
   }
 
+  //checking which has more unique characters
   if (bKeys.length < aKeys.length) {
     for (let i = 0; i < aKeys.length; i++) {
       if (bKeys.includes(aKeys[i])) {
@@ -42,9 +51,10 @@ function makeAnagram(a, b) {
     }
   }
 
+  //return the number of characters that do not form an anagram
   return Object.values(aObj).reduce((a,b) => a + b) + Object.values(bObj).reduce((a,b) => a + b);
 }
 
-console.log(makeAnagram('cde', 'abc')); //4 (the number of characters deleted)
+console.log(makeAnagram('cde', 'abc')); //4 (the number of characters that should be deleted)
 console.log(makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke')); //30
 
